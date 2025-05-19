319b3d62ee4b06a015ee8a06df6b2a9a65debf45
Antonio Borrero <antojoseabad@gmail.com>
2025-05-19 18:05:31 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,21 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  
+  let domains = [];
+
+  let pronoums = ["the", "our", "your"];
+  let adjs = ["great", "big", "impressive", "fabulous"];
+  let nouns = ["jogger", "racoon", "dog", "army", "house"];
+
+  for (let pronoum of pronoums){
+    for (let adj of adjs){
+      for (let noun of nouns){
+        let domain = pronoum + adj + noun + ".com"
+        domains.push(domain)
+      }
+    }
+  }
+  document.querySelector("#domain").innerHTML = domains.map(d => `<li>${d}</li>`).join(" ")
+  console.log(domains)
 };
